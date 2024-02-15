804203cd09731ee90d1b2aed6af55ca09195e2bf
augustofarias2 <augustofarias027@gmail.com>
2024-02-15 12:49:30 -0300

    creo base de grafos y modifico clasificador

--- create_base_grafos.py
+++ create_base_grafos.py
@@ -1,11 +1,14 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import os
+from rdflib import Graph, URIRef, RDF, RDFS
+import urllib.parse
 
 # Creamos un grafo dirigido
 G = nx.DiGraph()
 
 # Creamos nodos para las personas históricas de la ciudad de Rosario
-personas = ['Lionel Messi', 'Luciana Aymar', 'Alberto Olmedo', 'Juan Carlos Baglietto','Nicki Nicole','Fito Paez', "Migue Granados","Roberto Fontanarrosa", "Angel Di Maria", "Ernesto Che Guevara", "Lisandro de la Torre", "Cristian Newell"]
+personas = ['Lionel_Messi', 'Luciana_Aymar', 'Alberto_Olmedo', 'Juan_Carlos_Baglietto', 'Nicki_Nicole', 'Fito_Paez', 'Migue_Granados', 'Roberto_Fontanarrosa', 'Angel_Di_Maria', 'Ernesto_Che_Guevara', 'Lisandro_de_la_Torre', 'Cristian_Newell']
 
 # Creamos nodos para las ramas en las que se destacaron
 ramas = ['Deporte', 'Humor', 'Musica', 'Literatura', 'Politica']
@@ -23,27 +26,46 @@ G.add_nodes_from(personas, bipartite=0)  # personas
 G.add_nodes_from(ramas, bipartite=1)     # ramas
 
 # Creamos relaciones entre personas y ramas
-relaciones = [('Lionel Messi', 'Deporte'), ('Luciana Aymar', 'Deporte'),
-              ('Angel Di Maria', 'Deporte'),
-              ('Fito Paez', 'Musica'),
-              ('Nicki Nicole', 'Musica'), ('Juan Carlos Baglietto', 'Musica'),
-              ('Alberto Olmedo', 'Humor'), ('Migue Granados', 'Humor'),
-              ('Roberto Fontanarrosa', 'Humor'), ('Ernesto Che Guevara', 'Politica'),
-              ('Lisandro de la Torre', 'Politica'), ('Cristian Newell', 'Politica'),
-              ('Roberto Fontanarrosa', 'Literatura'), ('Alberto Olmedo', 'Literatura'),
-              ('Cristian Newell', 'Deporte'),
-              ('Ernesto Che Guevara', 'Literatura'), ('Lisandro de la Torre', 'Literatura')
-                ]
+relaciones = [('Lionel_Messi', 'Deporte'), ('Luciana_Aymar', 'Deporte'),
+             ('Angel_Di_Maria', 'Deporte'),
+             ('Fito_Paez', 'Musica'),
+             ('Nicki_Nicole', 'Musica'), ('Juan_Carlos_Baglietto', 'Musica'),
+             ('Alberto_Olmedo', 'Humor'), ('Migue_Granados', 'Humor'),
+             ('Roberto_Fontanarrosa', 'Humor'), ('Ernesto_Che_Guevara', 'Politica'),
+             ('Lisandro_de_la_Torre', 'Politica'), ('Cristian_Newell', 'Politica'),
+             ('Roberto_Fontanarrosa', 'Literatura'), ('Alberto_Olmedo', 'Literatura'),
+             ('Cristian_Newell', 'Deporte'),
+             ('Ernesto_Che_Guevara', 'Literatura'), ('Lisandro_de_la_Torre', 'Literatura')
+               ]
 
 # Agregamos relaciones al grafo
 G.add_edges_from(relaciones)
 
-#Guardar base de datos formato rdf
-nx.write_rdf(G, "test.rdf")
+#Crear un rdf con los datos
+g = Graph()
 
+# Agregamos las personas como instancias de la clase Persona
+for persona in personas:
+    persona_uri = urllib.parse.quote(persona)
+    g.add((URIRef(persona_uri), RDF.type, URIRef('http://www.w3.org/2002/07/owl#NamedIndividual')))
+    g.add((URIRef(persona_uri), RDFS.label, URIRef(persona)))
 
-# Dibujamos el grafo
-pos = nx.bipartite_layout(G, personas)
-nx.draw(G, pos, with_labels=True, node_size=500, node_color='lightblue')
-plt.title('Grafo de Personas Históricas y sus Ramas de Destaque')
-plt.show()
+# Agregamos las ramas como instancias de la clase Rama
+for rama in ramas:
+    rama_uri = urllib.parse.quote(rama)
+    g.add((URIRef(rama_uri), RDF.type, URIRef('http://www.w3.org/2002/07/owl#NamedIndividual')))
+    g.add((URIRef(rama_uri), RDFS.label, URIRef(rama)))
+
+# Agregamos las relaciones entre personas y ramas
+for relacion in relaciones:
+    g.add((URIRef(urllib.parse.quote(relacion[0])), URIRef('http://www.w3.org/2002/07/owl#hasRama'), URIRef(urllib.parse.quote(relacion[1])))
+    )
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bdd_grafos')
+
+#Guardar el grafo en formato tutle en la carpeta bdd_grafos
+g.serialize(destination=os.path.join(path, f'graph.ttl'), format='turtle')
+
+# Imprimir el grafo
+# pos = nx.spring_layout(G)
+# nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='grey')
+# plt.show()
